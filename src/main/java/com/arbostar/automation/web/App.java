99f4371a57eea67a2deb78effad4d73b9536999c
Yuri Kamotsky <kamockii1977@gmail.com>
2025-03-27 18:33:26 +0300

    init project

---
+++ src/main/java/com/arbostar/automation/web/App.java
@@ -0,0 +1,13 @@
+package com.arbostar.automation.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
